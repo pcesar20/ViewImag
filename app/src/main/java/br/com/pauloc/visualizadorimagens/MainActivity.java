d32516086cb67e84feda081ccf1b79873341075f
Paulo Cesar <paulocesar.araujo.ads@gmail.com>
2020-04-24 20:34:04 -0300

    view de imagens simples

---
+++ app/src/main/java/br/com/pauloc/visualizadorimagens/MainActivity.java
@@ -0,0 +1,47 @@
+package br.com.pauloc.visualizadorimagens;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final ImageView imageView1 = findViewById(R.id.imageView1);
+        final ImageView imageView2 = findViewById(R.id.imageView2);
+        Button btnImg1 = findViewById(R.id.button_img1);
+        Button btnImg2 = findViewById(R.id.button_img2);
+        btnImg1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView1.setVisibility(View.VISIBLE);
+                imageView2.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        btnImg2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Alert
+                imageView2.setVisibility(View.VISIBLE);
+                imageView1.setVisibility(View.INVISIBLE);
+
+            }
+        });
+
+        public void class Alert{
+            AlertDialog.Builder msgD = new AlertDialog.Builder(MainActivity.this);
+            msgD.setTitle("Imagem View");
+            msgD.setMessage("Lindo demais!");
+            msgD.setNeutralButton("Concordo", null);
+            msgD.show();
+        }
+    }
+}
